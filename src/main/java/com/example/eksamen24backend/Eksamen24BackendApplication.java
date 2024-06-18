6c79a000ee6d8ecdf7f57892a27c9a33a9ada668
Abed Rass <abd.assoum@outlook.com>
2024-06-18 20:45:27 +0200

    First Commit

---
+++ src/main/java/com/example/eksamen24backend/Eksamen24BackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.eksamen24backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Eksamen24BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Eksamen24BackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/eksamen24backend/Eksamen24BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eksamen24backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Eksamen24BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
